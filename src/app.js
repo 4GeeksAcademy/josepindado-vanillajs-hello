005e39628a2aec8f82a86994031b000b399ce1a8
Jose Pindado <joseangelpindado@gmail.com>
2024-10-07 18:04:58 +0000

    Second commit

--- src/app.js
+++ src/app.js
@@ -5,20 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-//Aqui hice link a mi boton cuando cada vez le des click se cambie la carta
+//--BOTON NUEVA CARTA--
 window.onload = function() {
-  document.getElementById("generate").addEventListener("click", generateCard);
+  document.getElementById("nuevaCarta").addEventListener("click", nuevaCarta);
 };
 // cree mi funcion que genera mi carta random
-function generateCard() {
-  let figura = Math.floor(Math.random() * 4); /// Aqui genero u numero random
+function nuevaCarta() {
+  let figura = Math.floor(Math.random() * 4);
   let arrFiguras = ["♦", "♥", "♠", "♣"];
-  let color = figura < 2 ? "red" : "black"; /// para cambiar de color use mi posicion en mi arrFiguras
+  let color = figura < 2 ? "red" : "black";
   figura = arrFiguras[figura];
-  let valor = Math.floor(Math.random() * 13) + 1; /// Generador random de los valores del centro de la carta
-  switch (
-    valor /// hice un switch para cambiar los valores
-  ) {
+  let valor = Math.floor(Math.random() * 13) + 1;
+  switch (valor) {
     case 1:
       valor = "A";
       break;
@@ -32,8 +30,10 @@ function generateCard() {
       valor = "K";
       break;
   }
-  document.getElementById("valor").innerText = valor; /// aqui llame al valor dentro del html
-  document.getElementById("valor").style.color = color; /// aqui le puse un color random al valor
+
+  document.getElementById("valor").innerText = valor;
+  document.getElementById("valor").style.color = color;
+
   let figuras = document.querySelectorAll(".figura");
   for (let i = 0; i < figuras.length; i++) {
     figuras[i].innerText = figura;
